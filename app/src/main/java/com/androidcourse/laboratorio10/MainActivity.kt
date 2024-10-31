3f2dd44746baa41ffba1922c8968dfa430b924ac
David-Belizario <david.belizario@tecsup.edu.pe>
2024-10-30 22:53:22 -0500

    MainActivity - App funcional

--- app/src/main/java/com/androidcourse/laboratorio10/MainActivity.kt
+++ app/src/main/java/com/androidcourse/laboratorio10/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.androidcourse.laboratorio10.Screens.SeriesApp
 import com.androidcourse.laboratorio10.ui.theme.Laboratorio10Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,24 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Laboratorio10Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SeriesApp()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Laboratorio10Theme {
-        Greeting("Android")
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    Laboratorio10Theme {
+//        Greeting("Android")
+//    }
+//}
--- app/src/main/java/com/androidcourse/laboratorio10/Screens/ScreenInicio.kt
+++ app/src/main/java/com/androidcourse/laboratorio10/Screens/ScreenInicio.kt
@@ -1,9 +1,18 @@
 package com.androidcourse.laboratorio10.Screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
-fun ScreenInicio(){
-    Text(text = "¡Bienvenido a la Pantalla de Inicio!")
+fun ScreenInicio() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Bienvenido a la Pantalla de ¡Inicio!")
+    }
 }
