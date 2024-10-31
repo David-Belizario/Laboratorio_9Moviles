9f2e79ccea12494f16a83bdab15bc07dcb2abc5f
David-Belizario <david.belizario@tecsup.edu.pe>
2024-10-30 22:28:16 -0500

    SeriesApp: Navegación entre las diferentes pantallas y la barra inferior con acciones y ScreenInicio

---
+++ app/src/main/java/com/androidcourse/laboratorio10/Screens/ScreenInicio.kt
@@ -0,0 +1,9 @@
+package com.androidcourse.laboratorio10.Screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ScreenInicio(){
+    Text(text = "¡Bienvenido a la Pantalla de Inicio!")
+}
